3dab9905ca948175d2599b02c6381f0fa3cd0017
David Jean <davidejeaniii@gmail.com>
2024-03-15 14:38:46 -0500

    fixed import

--- data_pipeline/functions/process_sales_data.py
+++ data_pipeline/functions/process_sales_data.py
@@ -1,7 +1,7 @@
 import os
 
 import awswrangler as wr
-from helpers.datacleaning import drop_na, rename_avg_price_to_price
+from helpers.datacleaning import drop_na, rename_avg_price_to_price, remove_trailing_zeros_from_df, update_online_spend_with_quantity, total_spend
 
 
 def process_sales_data(event, context):
